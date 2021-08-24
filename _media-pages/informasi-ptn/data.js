49f9a8f8e89a59501c239d7ba80094f704f90074
Riovano <riovano82@gmail.com>
2021-08-24 22:45:46 +0700

    Blog post able to change dynamically

---
+++ _media-pages/informasi-ptn/data.js
@@ -0,0 +1,38 @@
+const postData = [
+    {
+        title: 'Lorem ipsum dolor sit amet',
+        author: 'Cicero',
+        dateText: '18 Agustus 2021', //ex: 18 Agustus 2021
+        dateNum: '2021-08-18', //YYYY-MM-DD, ex: 2021-08-18
+        imgSrc: 'https://images.unsplash.com/photo-1629220608817-0802c373e110?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quasi omnis ex atque corrupti! Temporibus aliquam deleniti rerum amet beatae!',
+        url: '#',
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet',
+        author: 'Cicero',
+        dateText: '18 Agustus 2021', //ex: 18 Agustus 2021
+        dateNum: '2021-08-18', //YYYY-MM-DD, ex: 2021-08-18
+        imgSrc: 'https://images.unsplash.com/photo-1629220608817-0802c373e110?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quasi omnis ex atque corrupti! Temporibus aliquam deleniti rerum amet beatae!',
+        url: '#',
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet',
+        author: 'Cicero',
+        dateText: '18 Agustus 2021', //ex: 18 Agustus 2021
+        dateNum: '2021-08-18', //YYYY-MM-DD, ex: 2021-08-18
+        imgSrc: 'https://images.unsplash.com/photo-1629220608817-0802c373e110?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quasi omnis ex atque corrupti! Temporibus aliquam deleniti rerum amet beatae!',
+        url: '#',
+    },
+    {
+        title: 'Lorem ipsum dolor sit amet',
+        author: 'Cicero',
+        dateText: '18 Agustus 2021', //ex: 18 Agustus 2021
+        dateNum: '2021-08-18', //YYYY-MM-DD, ex: 2021-08-18
+        imgSrc: 'https://images.unsplash.com/photo-1629220608817-0802c373e110?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quasi omnis ex atque corrupti! Temporibus aliquam deleniti rerum amet beatae!',
+        url: '#',
+    },
+];
---
+++ _media-pages/sharing-pengalaman/script.js
@@ -0,0 +1,27 @@
+const articleContainer = document.querySelector('.article-container ul');
+
+renderPost();
+
+function renderPost() {
+    for (let i = 0; i < postData.length; i++) {
+        const li = document.createElement('li');
+        li.classList.add('article-list');
+        li.innerHTML = `
+            <article class="article">
+                <img class="article-img" src=${postData[i].imgSrc} alt="" />
+                <article class="article-content">
+                    <h2>${postData[i].title}</h2>
+                    <p>${postData[i].content}<a class="link" href="#"> Read&nbsp;more</a></p>
+                    <p>
+                        <small>
+                            ${postData[i].author} -
+                            <time datetime=${postData[i].dateNum}>${postData[i].dateText}</time>
+                        </small>
+                    </p>
+                </article>
+            </article>
+        `;
+
+        articleContainer.appendChild(li);
+    }
+}
